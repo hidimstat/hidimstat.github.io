9fc5bbd6ffd12cbfc8cceedfe2b99734d86b40be
jeromedockes <jerome@dockes.org>
2025-04-23 14:16:41 +0000

    Pushing the docs to dev/ for branch: pull/214, commit c23a6cff8829ce7740e460521ed2b1c7b828c79a

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.0b1.dev240+g98f41e6',
+    VERSION: '0.2.0b1.dev241+ge3ef2ec',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
