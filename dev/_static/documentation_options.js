b32113aee4bfb1c6742f9ebd66413c694661a8bb
hidimstat-circleci <hidimstat@circleci.com>
2025-04-24 14:16:20 +0000

    Pushing the docs to dev/ for branch: main, commit ba7338d770a5631d220d538c431a2960e455c103

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.0b1.dev216+g43b4ce3',
+    VERSION: '0.2.0b1.dev217+gba7338d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
